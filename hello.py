d034fa41ff00640968c6071b3006ba5ff23b0260
w <935462955@qq.com>
2019-04-11 05:11:27 +0800

    第四章表单 flask_wtf

--- hello.py
+++ hello.py
@@ -2,15 +2,27 @@ from flask import Flask,render_template
 from flask_bootstrap import Bootstrap #包含了jQuery
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+import wtforms
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '我是密钥'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(FlaskForm):
+    name = wtforms.StringField('what is your name?', validators = [DataRequired()])
+    submit = wtforms.SubmitField('Submit')
 
-@app.route('/')
+@app.route('/',methods = ['GET','POST'])
 def index():
-    return render_template('index.html',current_time = datetime.utcnow()) #utc是协调世界时间(coordinated universal time)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit(): #如果是一个post请求并且有效返回True ,初次打开页面时提交给服务器的是一个包含空表单的GET请求所以返回False
+        name = form.name.data 
+        form.name.data = '' #清空name
+    return render_template('index.html',current_time = datetime.utcnow(),form = form, name = name) #utc是协调世界时间(coordinated universal time)
 
 
 @app.route('/user/<name>')
