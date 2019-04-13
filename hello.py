a42601298c36d1893c6c37856da33f387464ee34
w <935462955@qq.com>
2019-04-14 00:50:51 +0800

    增加数据库

--- hello.py
+++ hello.py
@@ -1,15 +1,38 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_bootstrap import Bootstrap #包含了jQuery
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-import wtforms
+import wtforms,os,pymysql
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Cogi:123@129.204.25.212/cogi'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = '我是密钥'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    users = db.relationship('User',backref = 'role', lazy = 'dynamic')
+    
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64),unique =True, index = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
     name = wtforms.StringField('what is your name?', validators = [DataRequired()])
@@ -17,12 +40,20 @@ class NameForm(FlaskForm):
 
 @app.route('/',methods = ['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit(): #如果是一个post请求并且有效返回True ,初次打开页面时提交给服务器的是一个包含空表单的GET请求所以返回False
-        name = form.name.data 
-        form.name.data = '' #清空name
-    return render_template('index.html',current_time = datetime.utcnow(),form = form, name = name) #utc是协调世界时间(coordinated universal time)
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data,role_id = 3)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',current_time = datetime.utcnow(),form = form, name = session.get('name'),known = session.get('known',False)) #utc是协调世界时间(coordinated universal time)
 
 
 @app.route('/user/<name>')
